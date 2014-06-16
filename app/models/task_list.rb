18677715a35aadf443b7e3b95d701a8f29db23cd
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-16 09:12:06 -0400

    Added dependecies between task_list and tasks

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,6 +1,8 @@
 class TaskList < ActiveRecord::Base
   has_many :list_members
   has_many :users, through: :list_members
+  has_many :tasks, dependent: :destroy
+
 
   validates :name, presence: true
 end
